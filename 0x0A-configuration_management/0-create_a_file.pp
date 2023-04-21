243d116d0540b795b2af0e5c1ac60de8afa0f66a
kingswaywalsales <marco.nkululeko@gmail.com>
2023-04-21 15:28:53 -0700

    task2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Create file /tmp/holberton
+# Create a file using Puppet
+
 file { '/tmp/holberton':
-  path    => '/tmp/holberton',
-  mode    => '0744',
+  ensure  => 'present',
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  mode    => '0744'
 }
